49b1802a08732849143c0c37d4b962130a1f3d28
Brandon Barnes <brandon@thirdandgrove.com>
2017-12-15 15:03:01 -0800

    Refactor menu to also work as results.

--- js/objects/Asteroid.js
+++ js/objects/Asteroid.js
@@ -1,6 +1,6 @@
 var Asteroid = function (game, settings) {
   this.settings = Object.assign({
-    startY: 200, // in px
+    startY: 250, // in px
     startX: game.world.centerX, // in px
     verticalSpeed: 0,
     rotationSpeed: 0.05,
